51487784a67453f0a4d46b0703517c97d490e2aa
Morgan Wigmanich <okize123@gmail.com>
2013-05-19 23:43:35 -0400

    updates

--- template5/javascript/script.js
+++ template5/javascript/script.js
@@ -22,6 +22,7 @@ $(document).ready(function () {
     pinnedPanelLocation: 'top'
   });
 
+  // HeroCarousel component
   $('.heroCarousel').heroCarousel({
     navigationPosition: 'Outside',
     heroImageLink: true,
